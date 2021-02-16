6f71ed8e7ad8f6b22ed5007964e8845e93feb014
jHut-1 <71864493+jHut-1@users.noreply.github.com>
2021-02-16 18:46:45 -0500

    Update 1.py

--- 1.py
+++ 1.py
@@ -1,4 +1,6 @@
 compra = float(input("Digite el valor de su compra: "))
-iva = compra + (compra * 0.19)
+iva = compra * 0.19
+descuento = iva * 0.05
 
-print("El valor de su compra es: ", iva )
+print("El valor de su compra es: ", compra + iva + descuento)
+print("El valor neto de su compra ", compra)
